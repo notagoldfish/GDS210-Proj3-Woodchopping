52b0ff0aa5910ee7f4b6a0db7de566d4dfd370a0
notagoldfish <61007801+notagoldfish@users.noreply.github.com>
2020-12-01 22:08:36 +0800

    Axe is Chopping
    Axe chop chop chop

---
+++ Assets/BzKovSoft/ObjectSlicer/BzSliceSimpleMeshAdapter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace BzKovSoft.ObjectSlicer
+{
+	public class BzSliceSimpleMeshAdapter : IBzSliceAdapter
+	{
+		Vector3[] _vertices;
+
+		public BzSliceSimpleMeshAdapter()
+		{
+
+		}
+
+		public BzSliceSimpleMeshAdapter(Vector3[] vertices)
+		{
+			_vertices = vertices;
+		}
+
+		public Vector3 GetWorldPos(int index)
+		{
+			Vector3 position = _vertices[index];
+			return position;
+		}
+
+		public Vector3 GetWorldPos(BzMeshData meshData, int index)
+		{
+			return meshData.Vertices[index];
+		}
+
+		public bool Check(BzMeshData meshData)
+		{
+			return true;
+		}
+
+		public void RebuildMesh(Mesh mesh, Material[] materials, Renderer meshRenderer)
+		{
+			throw new NotSupportedException();
+		}
+
+		public Vector3 GetObjectCenterInWorldSpace()
+		{
+			return Vector3.zero;
+		}
+	}
+}
---
+++ Assets/Slicing Test/SlicerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BzKovSoft.ObjectSlicer;
+
+public class SlicerController : MonoBehaviour
+{
+
+    public GameObject _target;
+
+    void Start()
+    {
+        var sliceable = _target.GetComponent<IBzSliceable>();
+
+        if (sliceable != null)
+        {
+            print("sliceable does not equal null");
+            sliceable.Slice(new Plane(Vector3.up, 0), null);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
